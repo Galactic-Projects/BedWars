83b3b04adaff238a6faafc961f4049e29a96b66a
TntTastisch <tnttastisch@gmail.com>
2022-12-21 08:25:39 +0100

    GalacticAPI Update

--- src/main/java/net/galacticprojects/spigot/BedWars.java
+++ src/main/java/net/galacticprojects/spigot/BedWars.java
@@ -1,20 +1,34 @@
 package net.galacticprojects.spigot;
 
-import net.galacticprojects.spigot.channel.BungeeMessage;
 import net.galacticprojects.spigot.listener.BungeeMessageListener;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.Messenger;
 
 public class BedWars extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
-        this.getServer().getMessenger().registerIncomingPluginChannel(this, "BedWars", new BungeeMessageListener());
+        registerMessenger();
+        registerListener();
+        registerCommands();
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public void registerListener() {
+
+    }
+
+    public void registerCommands() {
+    }
+
+    public void registerMessenger() {
+        Messenger messenger = this.getServer().getMessenger();
+        messenger.registerOutgoingPluginChannel(this, "BungeeCord");
+        messenger.registerIncomingPluginChannel(this, "BedWars", new BungeeMessageListener());
     }
 }
